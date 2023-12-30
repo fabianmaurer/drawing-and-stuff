82a8c2fe390884d0a4e91b58e51ed6782d132fd0
fabianmaurer <maurerfabi@gmail.com>
2023-12-30 15:26:27 +0100

    Initial commit

---
+++ magic.js
@@ -0,0 +1,144 @@
+
+
+const srcImg = document.querySelector('#portrait'); 
+srcImg.crossOrigin = "Anonymous";
+const maxValue = 16;
+const maxLineLength = 10;
+let srcWidth;
+let srcHeight;
+let strokeOpacity = 0.1;
+const scalingFactor = 4;
+const normalize = false
+const delayed = true
+
+const canvas = document.querySelector('#main-canvas')
+
+const delay = time => new Promise(res=>setTimeout(res,time));
+
+waitForImage()
+
+function waitForImage() {
+    if(srcImg.width == 0) {
+        requestAnimationFrame(waitForImage)
+    }else{
+        srcWidth = srcImg.width
+        srcHeight = srcImg.height
+        let densityArray = getImageDensity(srcImg, maxValue)
+        
+        drawImage(canvas, densityArray, 'strokes', srcWidth, srcHeight, scalingFactor, 0, maxValue, maxLineLength)
+
+    }
+}
+
+function getImageDensity(img, maxValue) {
+    const _canvas = document.createElement('canvas'); 
+    const _ctx = _canvas.getContext('2d'); 
+     
+    _canvas.width = img.width; 
+    _canvas.height = img.height; 
+     
+    _ctx.drawImage(img, 0, 0); 
+     
+    const rgba = _ctx.getImageData( 
+      0, 0, img.width, img.height 
+    ).data;
+    
+    let _densityArray = []
+    const normalizationFactor = 1
+    if(normalize) {
+        let totalDensity = 0
+        for(let i=0; i<rgba.length; i=i+4) {
+            totalDensity += Math.round(getDensity(rgba[i],rgba[i+1],rgba[i+2], maxValue))
+        }
+    
+        const targetAverage = maxValue / 2
+        let averageDensity = totalDensity / (rgba.length / targetAverage)
+        normalizationFactor = targetAverage / averageDensity
+    }
+    for(let i=0; i<rgba.length; i=i+4) {
+        _densityArray.push(Math.min(maxValue,Math.round(normalizationFactor * getDensity(rgba[i],rgba[i+1],rgba[i+2], maxValue))))
+    }
+
+    return _densityArray
+}
+
+async function drawImage(canvas, density, method, width, height, scaling, timeperstep, maxValue, maxLineLength) {
+    canvas.width = width * scaling + scaling * 2
+    canvas.height = height * scaling + scaling * 2
+    const ctx = canvas.getContext('2d')
+    const gradient = ctx.createLinearGradient(0, 0, 1, -1)
+    gradient.addColorStop(0, '#ccc')
+    gradient.addColorStop(0.2, '#333')
+    gradient.addColorStop(0.6, '#333')
+    gradient.addColorStop(1, '#ccc')
+    ctx.strokeStyle = 'rgba(0,0,0,0.1)'
+    ctx.opacity = strokeOpacity
+    ctx.lineWidth = 1
+    let numlines = 0
+    // find first pixel w/ max value
+    while(maxValue > 0) {
+        for(let y=0; y<height; y++) {
+            for(let x=0; x<width; x++) {
+                if(density[x+y*width] == maxValue) {
+                    density[x+y*width]--
+                    let lineLength = 1;
+                    let lineLength2 = 0;
+                    for(let offset = 1; offset <= Math.min(x, height-y); offset++) {
+                        if(density[x-offset+(y+offset)*width] > Math.max(0,maxValue - 1)) {
+                            density[x-offset+(y+offset)*width]--;
+                            lineLength++;
+                            if(lineLength >= fuzzy(maxLineLength, 0.2) / 2)
+                                break;
+                        } else {
+                            break;
+                        }
+                    }
+                    for(let offset = 1; offset <= Math.min(width-x, y); offset++) {
+                        if(density[x+offset+(y-offset)*width] > Math.max(0,maxValue - 1)) {
+                            density[x-offset+(y+offset)*width]--;
+                            lineLength2++;
+                            if(lineLength2 >= fuzzy(maxLineLength, 0.2) / 2)
+                                break;
+                        } else {
+                            break;
+                        }
+                    }
+                    const randomness = 1.0
+                    xoff = 1 + (-0.5 + 1.0 * Math.random()) * randomness
+                    yoff = 1 + (-0.5 + 1.0 * Math.random()) * randomness
+                    rx1 = x + xoff + 1 * Math.random()
+                    ry1 = y + yoff + 1 * Math.random()
+                    rx2 = x + xoff + 1 * Math.random()
+                    ry2 = y + yoff + 1 * Math.random()
+                    await drawLine(ctx, scaling*(rx1+lineLength2), scaling*(ry1-lineLength2), scaling*(rx2-lineLength), scaling*(ry2+lineLength), timeperstep)
+                    numlines++
+                    document.querySelector('#status3').innerHTML = numlines
+                    if(delayed && numlines % 50 == 0)
+                        await timeout(10)
+                }
+            }
+        }
+        maxValue--;
+    }
+}
+
+async function drawLine(ctx, startX, startY, endX, endY, delay) {
+    ctx.beginPath()
+    ctx.moveTo(startX, startY)
+    ctx.lineTo(endX, endY)
+    ctx.stroke()
+    ctx.closePath()
+    return;
+}
+
+function getDensity(r,g,b,maxValue) {
+    return Math.floor((768-(r+g+b)) / (768/maxValue))
+}
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function fuzzy(num, range) {
+    return num - range + Math.random() * 2 * range
+}
